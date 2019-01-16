81f79ca16aa5c5b492964d212bfe49229e8103c6
varun2410 <varun241095@hotmail.com>
2019-01-16 12:24:33 +0530

    Add FeatureContext

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,57 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface;
+use Behat\Behat\Context\TranslatedContextInterface;
+use Behat\Behat\Context\BehatContext;
+use Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+
+//
+// Require 3rd-party libraries here:
+//
+//   require_once 'PHPUnit/Autoload.php';
+//   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Features context.
+ */
+class FeatureContext extends MinkContext
+{
+    /**
+     * Initializes context.
+     * Every scenario gets it's own context object.
+     *
+     * @param array $parameters context parameters (set them up through behat.yml)
+     */
+    public function __construct(array $parameters)
+    {
+        // Initialize your context here
+    }
+
+    /**
+     * @Then /^I should see Alert "([^"]*)"$/
+     */
+    public function iShouldSeeAlert($arg1)
+    {
+        PHPUnit_Framework_Assert::assertSame(
+            $arg1,
+            'Wrong Username Or Password'
+        );
+        $this->getSession()->getDriver()->getWebDriverSession()->accept_alert();
+    }
+//
+//    /**
+//     * @Then /^I should not see "([^"]*)"$/
+//     */
+//    public function iShouldNotSee($arg1)
+//    {
+//        PHPUnit_Framework_Assert::assertNotContains(
+//            $arg1,
+//            'My Profile'
+//        );;
+//    }
+
+}
