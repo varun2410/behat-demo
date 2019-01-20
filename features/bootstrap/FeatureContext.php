26eb9bee6f1654f278675fdfad155345e941eeb2
varun2410 <varun241095@hotmail.com>
2019-01-20 18:36:23 +0530

    Create api and bank survey scenario

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -20,6 +20,14 @@ use Behat\MinkExtension\Context\MinkContext;
  */
 class FeatureContext extends MinkContext
 {
+    private $headers;
+    private $jsonData;
+    private $verb;
+    private $username;
+    private $password;
+    private $url;
+    private $response;
+
     /**
      * Initializes context.
      * Every scenario gets it's own context object.
@@ -39,6 +47,116 @@ class FeatureContext extends MinkContext
         $this->getSession()->wait(2000);
     }
 
+    /**
+     * @Given /^I want to call "([^"]*)"$/
+     */
+    public function iWantToCall($arg1)
+    {
+        $this->url = $arg1;
+    }
 
+    /**
+     * @When /^I give set username as "([^"]*)" and password as "([^"]*)"$/
+     */
+    public function iGiveSetUsernameAsAndPasswordAs($arg1, $arg2)
+    {
+        $this->username = $arg1;
+        $this->password = $arg2;
+    }
+
+
+    /**
+     * @When /^I add in json data$/
+     */
+    public function iAddInJsonData()
+    {
+        $this->jsonData = '{
+	"Request": {
+		"satn": "123456",
+		"itemCount": "1",
+		"cardStatus": "Receive",
+		"transDate": "2018-09-06 05:11:00",
+		"merchantId": "915051220050178",
+        "terminalId": "90157866",
+        "cacoId": "05100010",
+        "referenceNo": "17098652134"
+	},
+	"CardInfo": [
+    {
+        "URN": "77667766"
+    }
+    ]
+}';
+    }
+
+    /**
+     * @Given /^I set verb as "([^"]*)"$/
+     */
+    public function iSetVerbAs($arg1)
+    {
+        $this->verb = $arg1;
+    }
+
+    /**
+     * @Given /^I create authorization header$/
+     */
+    public function iCreateAuthorizationHeader()
+    {
+        //content md5
+        $contentMd5 = base64_encode(md5($this->jsonData, true));
+
+        //content type
+        $contentType = 'application/json';
+
+        // Set the date timestamp header in UTC format
+        $dateNow = new DateTime('now',new DateTimeZone('UTC'));
+        $authdate = $dateNow->format('c');
+
+        // Form the string we use as part of the Authorization header
+        $stringToHash = $this->verb . "\n" . $contentMd5 ."\n" . $contentType . "\n" . $authdate . "\n";
+        $authCode = base64_encode(hash_hmac('sha1', $stringToHash, $this->password));
+        $authorization = 'RDN-CRM '.$this->username.':' . $this->password . ':'. $authCode;
+
+        $this->headers = [
+            'Content-Type' => 'application/json',
+            'Content-Length' => strlen($this->jsonData),
+            'Content-MD5' => $contentMd5,
+            'Authorization' => $authorization,
+            'Date' => $authdate,
+        ];
+    }
+
+    /**
+     * @Given /^I call the api$/
+     */
+    public function iCallTheApi()
+    {
+        $client = new GuzzleHttp\Client([
+            'base_uri' => $this->getMinkParameter('base_url')
+        ]);
+
+        $request_options = [
+            'headers' => $this->headers,
+            'body' => $this->jsonData,
+        ];
+
+        $response = $client->request(
+            $this->verb,
+            $this->url,
+            $request_options
+        );
+
+        $this->response = $response->getBody();
+    }
+
+
+    /**
+     * @Then /^I should get json response$/
+     */
+    public function iShouldGetJsonResponse()
+    {
+        $decodedResponse = json_encode(json_decode($this->response));
+        PHPUnit_Framework_Assert::assertNotContains('RDNException', $decodedResponse);
+    }
 
 }
--- features/crm.feature
+++ features/crm.feature
@@ -10,10 +10,19 @@ Feature: Log in and out of the site
     And I fill in "user_password" with "R3dd0tcrM123"
     And I press "Log In"
     Then I should see "Log Out"
-    And I should see "Home"
+    And I should see "Home".
 
   @javascript
-  Scenario: Logs in to the site
+  Scenario: Logs out of site
+    Given I am on "/index.php"
+    When I fill in "user_name" with "admin"
+    And I fill in "user_password" with "R3dd0tcrM123"
+    And I press "Log In"
+    Then I follow "Log Out"
+    And I should not see "Home"
+
+  @javascript
+  Scenario: Check for bank survey module
     Given I am on "/index.php"
     When I fill in "user_name" with "admin"
     And I fill in "user_password" with "R3dd0tcrM123"
@@ -23,3 +32,26 @@ Feature: Log in and out of the site
     And I wait sometime
     Then I should see " Search Bank Survey "
     And I should see "Create Bank Survey"
+
+  @javascript
+  Scenario: Check for bank survey module
+    Given I am on "/index.php"
+    When I fill in "user_name" with "admin"
+    And I fill in "user_password" with "R3dd0tcrM123"
+    And I press "Log In"
+    And I follow "Survey"
+    And I follow "Bank Survey"
+    And I wait sometime
+    And I fill in "accounts_bank_survey_1_name_basic" with "915058120050226"
+    And I press "search_form_submit"
+    And I wait sometime
+    Then I should see "181"
+
+  Scenario: Check ppv receive api
+    Given I want to call "/1.0/ppvlogistic"
+    When I give set username as "ppvuser" and password as "ppvpassword"
+    And I add in json data
+    And I set verb as "POST"
+    And I create authorization header
+    And I call the api
+    Then I should get json response
